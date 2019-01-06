a6ec61c510770bbbbc569cc6079e570d1741d6ec
Ryan Kubik <rmkubik@me.com>
2019-01-05 22:28:45 -0800

    Add rough xstate machine draft and implemenet onConnect/registerPlayer

--- src/js/screen/components/Screen.jsx
+++ src/js/screen/components/Screen.jsx
@@ -1,13 +1,81 @@
 import React, { Component } from 'react';
+import { Machine, actions } from 'xstate';
+import { interpret } from 'xstate/lib/interpreter';
 import MessageLog from '../../shared/components/MessageLog';
 
+const { assign } = actions;
 class Screen extends Component {
   state = {
     messages: [],
+    players: {},
   };
 
   constructor(props) {
     super(props);
+
+    const toggleMachine = Machine(
+      {
+        initial: 'loading',
+        context: {
+          players: {},
+          messages: [],
+        },
+        states: {
+          loading: {
+            on: {
+              registerPlayer: {
+                target: 'loading',
+                actions: 'registerPlayer',
+              },
+              start: 'waiting',
+            },
+          },
+          waiting: {
+            on: {
+              message: {
+                target: 'processing',
+                actions: 'recordMessage',
+              },
+            },
+          },
+          processing: {
+            invoke: {
+              src: 'processMessage',
+            },
+            onDone: {
+              target: 'waiting',
+            },
+          },
+        },
+      },
+      {
+        actions: {
+          registerPlayer: assign({
+            players: (ctx, event) => ({
+              ...ctx.players,
+              [event.deviceId]: {
+                action: undefined,
+              },
+            }),
+          }),
+          recordMessage: assign({
+            messages: (ctx, event) => [...ctx.messages, event.message],
+          }),
+        },
+        services: {
+          processMessage: (ctx, evt) => {
+            return Promise.resolve({});
+          },
+        },
+      },
+    );
+
+    const screenService = interpret(toggleMachine)
+      .onTransition(state => {
+        console.log('service state', state);
+        this.setState(state.context);
+      })
+      .start();
 
     // eslint-disable-next-line no-param-reassign
     props.airconsole.onMessage = (id, data) => {
@@ -21,14 +89,23 @@ class Screen extends Component {
         ],
       }));
     };
+
+    // eslint-disable-next-line no-param-reassign
+    props.airconsole.onConnect = deviceId => {
+      screenService.send({
+        type: 'registerPlayer',
+        deviceId,
+      });
+    };
   }
 
   render() {
-    const { messages } = this.state;
+    const { messages, players } = this.state;
 
     return (
       <div>
         <h1>Screen</h1>
+        <pre>{JSON.stringify(players, undefined, 2)}</pre>
         <MessageLog messages={messages} />
       </div>
     );
