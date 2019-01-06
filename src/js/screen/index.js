8f48f4928662002de199d9f22de441b0e9cc08cc
Ryan Kubik <rmkubik@me.com>
2019-01-05 18:30:44 -0800

    Eslint fix screen

--- src/js/screen/index.js
+++ src/js/screen/index.js
@@ -1,29 +1,27 @@
-import AirConsole from "../../../vendor/js/airconsole-1.7.0";
+import AirConsole from '../../../vendor/js/airconsole-1.7.0';
 
-var airconsole = new AirConsole();
+const airconsole = new AirConsole();
 
 let winner;
-let actions = {};
+const actions = {};
 
-airconsole.onConnect = device_id => {
-  actions[device_id] = "";
+airconsole.onConnect = deviceId => {
+  actions[deviceId] = '';
 };
 
 // Listen for messages from other devices
-airconsole.onMessage = function(device_id, data) {
+airconsole.onMessage = (deviceId, data) => {
   // We receive a message -> Send message back to the device
-  airconsole.message(device_id, "Recorded action!");
+  airconsole.message(deviceId, 'Recorded action!');
 
-  if (actions[device_id] === "") {
-    actions[device_id] = data;
+  if (actions[deviceId] === '') {
+    actions[deviceId] = data;
   }
 
   // Show message on device screen
-  var info = document.createElement("DIV");
-  const allActionsEntered = Object.values(actions).every(value => value !== "");
+  const info = document.createElement('DIV');
+  const allActionsEntered = Object.values(actions).every(value => value !== '');
 
-  if (allActionsEntered) {
-  }
   info.innerHTML = JSON.stringify(actions, undefined, 2);
   document.body.appendChild(info);
 };
